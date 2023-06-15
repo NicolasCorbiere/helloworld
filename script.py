f8c7b5dcfb75f43d3decc2ed5ff99856997012f6
ncorbiere <nicolas.corbiere@amadeus.com>
2023-06-15 08:52:27 +0000

    add comments

--- script.py
+++ script.py
@@ -11,13 +11,15 @@ def play_fizzbuzz(maxValue: int) -> None:
 
 
 if __name__ == "__main__":
-    play_fizzbuzz(2.5)
+    play_fizzbuzz(25)
 
 # add comments and tests
 # don't use a whilem don't print the number if there is a Fizz, a Buzz or both
 # add 7, 11
 
 # use venv, black and mypy
+# import script.py
 # . venv/bin/activate
+# pip install black
 # black script.py
 # mypy --strict script.py 
